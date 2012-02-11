73699a88424661d9a404c337a409d928be66600f
rschupp <rschupp@13bdeeb7-1ef5-0310-ba8f-c9f358a9e96d>
2012-02-11 20:27:55 +0000

    run run_with_inc.pl using a nonce PAR_TMPDIR, too (cf. 1331)

--- myldr/run_with_inc.pl
+++ myldr/run_with_inc.pl
@@ -1,4 +1,8 @@
 #!perl
+
+use File::Temp;
+$ENV{PAR_TMPDIR} = File::Temp::tempdir(TMPDIR => 1, CLEANUP => 1);
+
 # add -I options corresponding to @INC after the first element of @ARGV,
 # then execute it
 splice @ARGV, 1, 0, map { "-I$_" } @INC; 
