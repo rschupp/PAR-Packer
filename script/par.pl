45f2cd5ac52d4bbc5b4bb8e92b7f6ec7b8a12d4e
rschupp <roderich.schupp@gmail.com>
2022-06-30 18:26:18 +0200

    Fixes #62: rs6000_71 (AIX) "An offset in the .loader section header is too large."
    Communicate pp option "--clean" to the generated executable in a
    different way. Previously this was done by patching "__ENV_PAR_..." strings
    **in the executable being built** and interpreting these strings in
    par.pl **at runtime**. Though hacky, this seemingly worked on any OS
    (without rendering the executable invalid).
    But the only information ever passed was PAR_CLEAN and this was gleaned
    at build time from the value of $par.clean (as JSONPath) from META.yml
    in the zip (set by pp by option "--clean").
    Instead readi and interprete "META.yml" in par.pl **at runtime**.

--- script/par.pl
+++ script/par.pl
@@ -474,14 +474,6 @@ if ($out) {
     }
 
 
-    my %env = do {
-        if ($zip and my $meta = $zip->contents('META.yml')) {
-            $meta =~ s/.*^par:$//ms;
-            $meta =~ s/^\S.*//ms;
-            $meta =~ /^  ([^:]+): (.+)$/mg;
-        }
-    };
-
     # Open input and output files {{{
 
     if (defined $par) {
@@ -515,15 +507,7 @@ if ($out) {
         require PAR::Filter::PodStrip;
         PAR::Filter::PodStrip->apply(\$loader, $0);
     }
-    foreach my $key (sort keys %env) {
-        my $val = $env{$key} or next;
-        $val = eval $val if $val =~ /^['"]/;
-        my $magic = "__ENV_PAR_" . uc($key) . "__";
-        my $set = "PAR_" . uc($key) . "=$val";
-        $loader =~ s{$magic( +)}{
-            $magic . $set . (' ' x (length($1) - length($set)))
-        }eg;
-    }
+
     $fh->print($loader);
     # }}}
 
@@ -667,6 +651,16 @@ if ($out) {
     ( $zip->readFromFileHandle($fh, $progname) == Archive::Zip::AZ_OK() )
         or die qq[Error reading zip archive "$progname"];
     Archive::Zip::setChunkSize(64 * 1024);
+
+    if (my $meta = $zip->contents('META.yml')) {
+        # check par.clean
+        $meta =~ s/.*^par:\s*$//ms;
+        $meta =~ s/^\S.*//ms;
+        if (my ($clean) = $meta =~ /^\s+clean: (.*)$/m) {
+            $clean =~ /^\s*|\s*$/g;
+            $ENV{PAR_CLEAN} = 1 if $clean;
+        }
+    }
 
     push @PAR::LibCache, $zip;
     $PAR::LibCache{$progname} = $zip;
@@ -912,20 +906,15 @@ sub _par_init_env {
     }
 
     for (qw( SPAWNED TEMP CLEAN DEBUG CACHE PROGNAME ) ) {
-        delete $ENV{'PAR_'.$_};
-    }
-    for (qw/ TMPDIR TEMP CLEAN DEBUG /) {
-        $ENV{'PAR_'.$_} = $ENV{'PAR_GLOBAL_'.$_} if exists $ENV{'PAR_GLOBAL_'.$_};
+        delete $ENV{"PAR_$_"};
     }
 
-    my $par_clean = "__ENV_PAR_CLEAN__               ";
+    for (qw/ TMPDIR TEMP CLEAN DEBUG /) {
+        $ENV{"PAR_$_"} = $ENV{"PAR_GLOBAL_$_"} if exists $ENV{"PAR_GLOBAL_$_"};
+    }
 
     if ($ENV{PAR_TEMP}) {
         delete $ENV{PAR_CLEAN};
-    }
-    elsif (!exists $ENV{PAR_GLOBAL_CLEAN}) {
-        my $value = substr($par_clean, 12 + length("CLEAN"));
-        $ENV{PAR_CLEAN} = $1 if $value =~ /^PAR_CLEAN=(\S+)/;
     }
 }
 
